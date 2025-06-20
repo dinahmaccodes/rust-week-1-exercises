8cf61b5112db51be53e96e7258cafe7678683196
dinahmaccodes <dinahmaccodes@gmail.com>
2025-06-20 16:51:49 +0100

    fix build errors

--- src/lib.rs
+++ src/lib.rs
@@ -21,11 +21,3 @@ pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
 
     Ok(version)
 }
-
-fn main() {
-    let extract = extract_tx_version("02000000000101706dc474338179f4ab8b7f0a4d07a2050113d7a0a9d21162e98b7319b102d3050100000000fdffffff02c9e10100000000001600148744bf9d300850a598b1a891f9a8d66524a4773065fc000000000000160014d1fae9a4de635c9c2e576238251d71be28a34dff0247304402201bf91432bbb345dcaa883a14fb7f18df7c821b160cc693f242112ba1a0acbdeb0220541b082c5fd4174f8eae782e213c1ebfc87b0598740ee0ef8463474debe83817012102062aea304064469ed250f46622e411de7eff4f07703e4273df6c80d58954ac2f00000000"); // Example hex
-    match extract {
-        Ok(version) => println!("Transaction version: {}", version),
-        Err(e) => println!("Error: {}", e),
-    }
-}
